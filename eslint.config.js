ff0b95eecd0e65ad06c9660151a3484619f7e876
Daniil Batiuk <sportak50@gmail.com>
2025-07-02 12:09:58 +0300

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import js from '@eslint/js'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-require-imports': 'warn',
+      'no-console': 'warn',
+      'prefer-const': 'warn',
+      'react/jsx-no-undef': 'error',
+      'no-undef': 'off',
+      'node/prefer-global/process': 'off',
+      'import/consistent-type-specifier-style': 'off',
+      'perfectionist/sort-named-imports': 'off',
+      'no-undef-init': 'off',
+      eqeqeq: 'warn',
+      'prefer-template': 'warn',
+      'perfectionist/sort-imports': 'off',
+      'jsonc/sort-keys': 'off',
+      'unicorn/prefer-number-properties': 'off',
+    },
+  },
+])
